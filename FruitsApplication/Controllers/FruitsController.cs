49fa2c32c92d7deddbc941d7f8f4228d6fa42b4b
Your Name <you@example.com>
2021-04-09 07:52:35 +0530

    Fruits Document

---
+++ FruitsApplication/Controllers/FruitsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FruitsApplication.Controllers
+{
+    public class FruitsController : Controller
+    {
+        // GET: Fruits
+
+        public ActionResult index()
+        {
+            return View();
+        }
+        public ActionResult SideManu()
+        {
+            return PartialView("_Commonfolder");
+        }
+        public ActionResult Amrud()
+        {
+            return View();
+        }
+        public ActionResult Mango()
+        {
+            return View();
+        }
+        public ActionResult Apple()
+        {
+            return View();
+        }
+        public ActionResult Banana()
+        {
+            return View();
+        }
+        public ActionResult Pineapple()
+        {
+            return View();
+        }
+        public ActionResult Grapes()
+        {
+            return View();
+        }
+       
+    }
+} 
---
+++ FruitsApplication/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FruitsApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Globle()
+        {
+            return View();
+        }
+        public ActionResult Local()
+        {
+            return View();
+        }
+        public ActionResult Wide()
+        {
+            return View();
+        }
+
+       
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
